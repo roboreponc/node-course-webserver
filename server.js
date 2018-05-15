4bb4b796fc98329c146835860eae4cda8c790907
Roger Bolser <carydeveloper@gmail.com>
2018-05-15 15:53:51 -0400

    Initial Commit

---
+++ server.js
@@ -0,0 +1,73 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+let app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+
+app.use((req, res, next) => {
+  let now = new Date().toString();
+  let log = `${now}: ${req.method} ${req.url}`
+  console.log(log);
+  fs.appendFile('server.log', log + '\n', (err) => {
+    if (err) {
+      console.log('Unable to append server.log');
+    }
+  })
+  next();
+});
+
+// app.use((req, res, next) => {
+//   res.render('maintenance.hbs');
+// });
+
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear();
+})
+
+hbs.registerHelper('screamIt', (text) => {
+  return text.toUpperCase();
+})
+
+// app.get('/', (req, res) => {
+//   // res.send('<h1>Hello Express!</h1>');
+//   res.send({
+//     name: 'Roger',
+//     likes: [
+//       'Shooting',
+//       'Music',
+//       'Movies'
+//     ]
+//   });
+// });
+
+app.get('/', (req, res) => {
+  // res.send('About Page');
+  res.render('home.hbs', {
+    pageTitle: 'Home Page',
+    welcomeMessage: 'Welcome to the Webserver Home Page'
+  });
+});
+
+
+app.get('/about', (req, res) => {
+  // res.send('About Page');
+  res.render('about.hbs', {
+    pageTitle: 'About Page'
+  });
+});
+
+app.get('/bad', (req, res) => {
+    res.send({
+      msg: '<h1>Who Knew</h1>',
+      error: 'This is a BAD page'
+    });
+});
+
+app.listen(3000, () => {
+  console.log('server is up on port 3000');
+});
